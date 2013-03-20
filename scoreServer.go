e299503b5485b9838b902878077c7a79527a3d30
Anthony Pardee <apardee@gmail.com>
2013-03-19 19:59:16 -0500

    server updates

--- scoreServer.go
+++ scoreServer.go
@@ -3,6 +3,7 @@ package main
 import "io"
 import "fmt"
 import "time"
+import "strconv"
 import "net/http"
 import "encoding/json"
 import "github.com/garyburd/redigo/redis"
@@ -11,24 +12,41 @@ var pool redis.Pool
 
 type HighScore struct {
 	Score float64
-	Time int64
+	Time  int64
 }
 
+// Http request handler for posting and getting high scores.
 func HandleScore(w http.ResponseWriter, req *http.Request) {
 	var out string
+
+	// Connect to the data source.
+	conn := pool.Get()
+	defer conn.Close()
+
+	outVal, err := redis.Bytes(conn.Do("GET", "highScore:value"))
+	if err != nil {
+		return	
+	}
+
+	highScore, err := strconv.ParseFloat(string(outVal), 64)
+	if err != nil {
+		return
+	}
+
 	if req.Method == "GET" {
-		var err interface {}
-		conn := pool.Get()
-		highScore, err := redis.Int(conn.Do("GET", "highScore:value"))
+		// Retrieve the high score from the server and format it as json.
 		if err == nil {
-			currentHigh := HighScore{ float64(highScore), 0 }
+			currentHigh := HighScore{float64(highScore), 0}
 			marshaled, _ := json.Marshal(currentHigh)
 			out = string(marshaled)
 		}
-	} else {
-		out = req.FormValue("scorePosted")
+	} else if req.Method == "POST" {
+		postedScore, err := strconv.ParseFloat(req.FormValue("scorePosted"), 64)
+		if err == nil && postedScore > highScore {
+			conn.Do("SET", "highScore:value", postedScore)
+		}
 	}
-	
+
 	io.WriteString(w, out)
 }
 
@@ -45,13 +63,13 @@ func setupConnectionPool() redis.Pool {
 		_, err := c.Do("PING")
 		return err
 	}
-	
+
 	// Initialize the redis connection pool used by the score handler.
-	pool := redis.Pool {
-		MaxIdle : 10,
-		IdleTimeout: 300 * time.Second, 
-		Dial: dialFunc,
-		TestOnBorrow : testOnBorrow,
+	pool := redis.Pool{
+		MaxIdle:      250,
+		IdleTimeout:  300 * time.Second,
+		Dial:         dialFunc,
+		TestOnBorrow: testOnBorrow,
 	}
 
 	return pool
@@ -60,7 +78,9 @@ func setupConnectionPool() redis.Pool {
 func main() {
 	pool = setupConnectionPool()
 
-	http.HandleFunc("/", HandleScore)
-	fmt.Println("Began serving requests at: 127.0.0.1:4000", );
+	http.Handle("/", http.FileServer(http.Dir("./public")))
+	http.HandleFunc("/scores", HandleScore)
+
+	fmt.Println("Began serving requests at: 127.0.0.1:4000")
 	http.ListenAndServe("localhost:4000", nil)
 }
